bf4fb68270a6ec4244555c41557a7a0641e86755
ARG <el_propio_alex@hotmail.com>
2025-09-15 00:57:15 +0200

    SQL - selectFrom

--- src/db.ts
+++ src/db.ts
@@ -1,71 +1,83 @@
+// Table
 export type UserTable = {
-  id: string;
-  firstName: string;
-  lastName: string;
-  birthDate: Date;
-};
-export type CompanyTable = {
-  id: string;
-  name: string;
+    id: string;
+    firstName: string;
+    lastName: string;
+    birthDate: Date;
 };
 
+// Table
+export type CompanyTable = {
+    id: string;
+    name: string;
+};
+
+// DB
 export type CustomerDatabase = {
-  users: UserTable;
-  companies: CompanyTable;
+    users: UserTable;
+    companies: CompanyTable;
 };
 
 type Price = number;
 
+// Table
 export type ProductTable = {
-  id: string;
-  name: string;
-  description: string;
-  unitPrice: Price;
+    id: string;
+    name: string;
+    description: string;
+    unitPrice: Price;
 };
 
+// Table
 export type CartTable = {
-  id: string;
-  items: ProductTable["id"][];
+    id: string;
+    items: ProductTable["id"][];
 };
 
+// DB
 export type ShoppingDatabase = {
-  carts: CartTable;
-  products: ProductTable;
+    carts: CartTable;
+    products: ProductTable;
 };
 
 export const buildContext = <DB>() => {
-  return {
-    $db: undefined as DB
-  };
+    return {
+        $db: undefined as DB
+    };
 };
 
-export const selectFrom = (ctx: any, tableName: any) => ({
-  ...ctx,
-  _operation: "select",
-  _table: tableName,
+type CtxDb<DB> = { $db: DB };
+
+export const selectFrom = <
+    CTX extends CtxDb<any>,
+    TABLE extends keyof CTX['$db']
+>(ctx: CTX, tableName: TABLE) => ({
+    ...ctx,
+    _operation: "select" as const,
+    _table: tableName,
 });
 
 export const selectFields = (ctx: any, fieldNames: any[]) => ({
-  ...ctx,
-  _fields: fieldNames,
+    ...ctx,
+    _fields: fieldNames,
 });
 
 export const selectAll = (ctx: any) => ({
-  ...ctx,
-  _fields: "ALL",
+    ...ctx,
+    _fields: "ALL",
 });
 
 export const where = (ctx: any, field: any, operator: "=", value: any) => ({
-  ...ctx,
-  _where: {
-    field,
-    operator,
-    value,
-  },
+    ...ctx,
+    _where: {
+        field,
+        operator,
+        value,
+    },
 });
 
 export const deleteFrom = (ctx: any, tableName: any) => ({
-  ...ctx,
-  _operation: "delete",
-  _table: tableName,
+    ...ctx,
+    _operation: "delete",
+    _table: tableName,
 });
